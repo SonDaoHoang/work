ee892b2f9798a398095a1d828f9c7c3aaf3dbcc1
unknown <sondaohoang@vccorp.vn>
2021-10-06 17:17:16 +0700

    update Program.cs

--- Array/Program.cs
+++ Array/Program.cs
@@ -15,7 +15,7 @@ namespace Array
             {
                 Console.WriteLine("1.Xoa" + "\n" + "2.Sua" + "\n" + "3.Chen" + "\n" + "4.Hien thi" + "\n" + "5.Thoat");
                 var key = Console.ReadLine();
-                Function.CheckInt(key);
+                key = Function.CheckInt(key);
                 if (int.Parse(key) == 1)
                 {
                     MyList = Function.Xoa(MyList);
